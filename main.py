0eb1b8ca68b64df533b08b5167ecc72dd3f9fc82
Ashokreddy12 <aavula.ashok@gmail.com>
2021-01-09 18:45:52 +0530

    Improvised CSS and updated new background

--- main.py
+++ main.py
@@ -1,67 +1,66 @@
 # importing the necessary dependencies
-from flask import Flask, render_template, request,send_file,jsonify
-from flask_cors import CORS,cross_origin
+from flask import Flask, render_template, request, send_file, jsonify
+from flask_cors import CORS, cross_origin
 
 import pandas as pd
-#import seaborn as sns
-#import matplotlib
-#matplotlib.use('Agg')
-#import matplotlib.pyplot as plt
-#import os
-#sns.set()
+
 import pickle
 
-app = Flask(__name__) # initializing a flask app
+# initialize the flask app
+app = Flask(__name__)
 
-@app.route('/',methods=['GET'])  # route to display the home page
+
+@app.route('/', methods=['GET'])  # Route to display the home page
 @cross_origin()
 def homePage():
     return render_template("index.html")
 
-@app.route('/predict',methods=['POST','GET']) # route to show the predictions in a web UI
+
+@app.route('/predict', methods=['POST', 'GET'])  # Route to show the predictions in UI
 @cross_origin()
 def index():
     if request.method == 'POST':
         try:
-            #  reading the inputs given by the user
-            Pregnancies=float(request.form['Pregnancies'])
+            #  Get the inputs entered by user
+            Pregnancies = float(request.form['Pregnancies'])
             Glucose = float(request.form['Glucose'])
             BloodPressure = float(request.form['BloodPressure'])
             SkinThickness = float(request.form['SkinThickness'])
             Insulin = float(request.form['Insulin'])
-            bmi = float(request.form['BMI'])
-            Diabetes_Pedigree_Function = float(request.form['DiabetesPedigreeFunction'])
+            bmi = float(request.form['bmi'])
+            Diabetes_Pedigree_Function = float(request.form['Diabetes_Pedigree_Function'])
             Age = float(request.form['Age'])
 
             filename = 'modelForPrediction.sav'
-            loaded_model = pickle.load(open(filename, 'rb')) # loading the model file from the storage
+            loaded_model = pickle.load(open(filename, 'rb'))  # loading the model file from the storage
 
-            #loading Scaler pickle file
-            scaler = pickle.load(open('standardScalar.sav', 'rb'))
+            # Load Standard Scalar pickle file
+            scalar = pickle.load(open('standardScalar.sav', 'rb'))
 
-            # predictions using the loaded model file and scalar file
-            prediction = loaded_model.predict(scaler.transform([[Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, bmi, Diabetes_Pedigree_Function, Age]]))
+            # Predictions using the loaded model file and scalar file
+            prediction = loaded_model.predict(scalar.transform([[Pregnancies, Glucose, BloodPressure, SkinThickness,
+                                                                 Insulin, bmi, Diabetes_Pedigree_Function, Age]]))
 
             print('prediction is', prediction)
             # showing the prediction results in a UI
-            if prediction==1:
-                prediction = 'You are Diabetic!'
-                return render_template('results.html', prediction=prediction)
+            if prediction == 1:
+                prediction = 'You Are A Diabetic Patient.'
+                return render_template('diabetes.html', prediction=prediction)
             else:
-                prediction = 'You are not Diabetic!'
-                return render_template('results.html', prediction=prediction)
+                prediction = 'You Are Not A Diabetic Patient.'
+                return render_template('no_diabetes.html', prediction=prediction)
 
         except Exception as e:
-            print('The Exception message is: ',e)
-            return 'something is wrong'
+            print('The Exception message is: ', e)
+            return 'Something went wrong. Please check the code!!'
 
     else:
         return render_template('index.html')
 
 
 if __name__ == "__main__":
-    #to run locally
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    # To run in local
+    app.run(host='127.0.0.1', port=1229, debug=True)
 
-    #to run on cloud
-	#app.run(debug=True) # running the app
+    # To run in cloud
+    # app.run(debug=True) # running the app
